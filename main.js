3c2cf65211b91057b89d39db5169ebd65ce52bca
marcoMusicProgrammer <marco@MacBook-Pro-di-Marco.local>
2024-12-05 00:20:52 +0100

    update 2

--- main.js
+++ main.js
@@ -1,64 +1,82 @@
-function newInstr(form,p,OSCdiv,index) {
+const audioContext = new AudioContext()
 
-  inst[index] = "sample"
+const newSine = document.getElementById("new_sine")
+const delSine = document.getElementById("del_sine")
 
-    counter2 = (counter2 + 1)
-    console.log(inst)
+const finalMaster = audioContext.createGain()
+finalMaster.connect(audioContext.destination)
 
-    selettore = "sample"
+let selettore = undefined
 
-    form.remove()
-    p.remove()
-
-    ampSamples.push(ampInit)
-    envSamples.push([1,1])
-
-    amps.push(ampInit)
-    freqs.push(freqInit)
-    envs.push([1,1])
-
-    bufContainer(OSCdiv,counter2,index)
-    seqContainer(index,counter2)
-
-};
-
-function bufContainer(OSCdiv,counterSample,index) {
-
-  let AMPdiv = document.createElement("div")
-  AMPdiv.className = "amp_div"
-
-  let FREQdiv = document.createElement("div")
-  FREQdiv.className = "freq_div"
-
-
-  let PANdiv = document.createElement("div")
-  PANdiv.className = "pan_div"
-
-  let BUTTONdiv = document.createElement("div")
-  BUTTONdiv.className = "button_div"
-
-  let INSTname = document.createElement("h3")
-  INSTname.innerHTML = `Instrument ${index+1}`
-
-  OSCdiv.append(INSTname,AMPdiv,PANdiv,FREQdiv,BUTTONdiv)
+var inst = []
+var sample = []
+var gainSamples = []
+var ampSamples = []
+var envSamples = []
+var shifter = []
+var amps = []
+var bpf = []
+var freqs = []
+var pans = []
+var envs = []
+var oscs = []
+var gains = []
+var oscType = []
+var env = true
+var FMOscs = []
+var FMGains = []
+var FMfreq = []
+var FMamp = []
+var fm = true
+var panSamples = []
 
 
-  bufButton(counterSample,BUTTONdiv)
-  ampSample(counterSample,AMPdiv,amps[index])
-  pitchShift(counterSample,FREQdiv)
-  panBufSlider(counterSample,PANdiv)
+var bpm = 0
+
+// SEQUENCER
+
+let arrayCollectionOsc = {}
+let arrayCollectionSample = {}
+let arraySeqSample = {}
+let arraySeqOsc = {}
+let arraySeqOnOffSample = {}
+let arraySeqOnOffOsc = {}
+
+var ampInit = [0.9,0.5,0.1]
+var bpfInit = [1,0.5,2,1]
+var freqInit = 200
+var counter = -1
+var counter2 = -1
+var counter3 = -1
+
+
+function newInst() {
+    counter3 = (counter3 + 1)
+
+    console.log("primo contatore"+counter3)
+    selector(counter3)
+}
+
+function delInst() {
+
+    console.log("INDICE counter3: "+ counter3)
+
+
+    deleteInst(counter3)
+
+    if(counter3 > -1){
+        counter3 = counter3 - 1
+    }
+    if(counter > -1){
+        counter = counter - 1
+    }
+    if(counter2 > -1){
+        counter2 = counter2 - 1
+    }
+
 
 }
 
-function bufButton(index,BUTTONdiv) {
 
-  let table = document.createElement("table")
-  let tr = document.createElement("tr")
-  let td = document.createElement("td")
-  let td2 = document.createElement("td")
-
-  const inputAudio = document.createElement("input")
-  inputAudio.type = "file"
-  inputAudio.id = `fileInput${index}`
-  // inputAudi
-}
+newSine.addEventListener("click",newInst)
+delSine.addEventListener("click",delInst)
